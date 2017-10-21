7cb3ab81d1b32643b4a771f5cb3276a62a42bcd2
Birhanu Hailemariam <birhanuh@gmail.com>
2017-10-21 14:58:22 +0300

    Add css, fonts, images folders. Install file-loader, style-loader, css-loader, html-webpack-plugin and clean-webpack-plugin modules.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import _ from 'lodash';
 
+import './css/app.scss'
+
+import avatarPlaceholder from './images/avatar-placeholder.png'
+
+import printMe from './print.js'
+
 function component() {
   var element = document.createElement('div')
+  var btn = document.createElement('button')
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.classList.add('hello')
+
+  // Add avatar to div
+  let avatar = new Image()
+  avatar.src = avatarPlaceholder
+
+  btn.innerHTML = 'Click me'
+  btn.onclick = printMe
+
+  element.appendChild(avatar)
+  element.appendChild(btn)
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,46 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },  
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
 }
