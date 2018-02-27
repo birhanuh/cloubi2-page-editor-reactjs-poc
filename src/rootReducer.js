10811f920c5964a8c56d2a147241a982fb56d69a
Birhanu Hailemariam <birhanuh@gmail.com>
2018-02-27 13:42:24 +0200

    Update rootReducer.

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux'
 
 import pages from './reducers/page_editor/pages'
+import pages2 from './reducers/page_editor/pages2'
+import pages3 from './reducers/page_editor/pages3'
 
 export default combineReducers({
-  pages
+  pages,
+  pages2,
+  pages3
 })
